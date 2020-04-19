3c9438d11b73970587f6f763e4d85f58bb49fccf
VeganKebab <33206742+VeganKebab@users.noreply.github.com>
2020-04-19 19:38:25 +0300

    Attack anim

---
+++ Assets/Scripts/AttackScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public class AttackScript : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+
+    public float attackRange = 0.5f;
+
+    public LayerMask enemyLayers;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AttackFunction();
+        }
+    }
+
+    void AttackFunction()
+    {
+        animator.SetTrigger("Attack1");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("we hit:");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
